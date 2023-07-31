ef13c0c3905f46e0e3d5aee33380bb2d369062f6
apasyniuk <58225282+apasyniuk@users.noreply.github.com>
2023-07-31 19:17:01 +0200

    Add artificial resource in order to wait for cluster to be in ready state (#36)
    * Add artificial resource in order to wait for cluster to be in ready state
    
    This commit introduces change that will block module completion
    up until created cluster is in `Reade` state.
    
    Natural candidate for cluster ready state is `castai_gke_cluster` resource creation step.
    Hoverer as cluster resource needs to finish in order to run helm charts
    that actually deploys components that makes cluster to transit to ready state we
    need to introduce custom resource that will depend on helm charts
    resources that will pool ready state.
    
    To accomplish this `null_resource` was introduced that pools created
    cluster status until it is in ready state.

--- variables.tf
+++ variables.tf
@@ -4,6 +4,13 @@ variable "api_url" {
   default     = "https://api.cast.ai"
 }
 
+variable "castai_api_token" {
+  type = string
+  description = "Optional CAST AI API token created in console.cast.ai API Access keys section. Used only when `wait_for_cluster_ready` is set to true"
+  sensitive = true
+  default = ""
+}
+
 variable "project_id" {
   type        = string
   description = "The project id from GCP"
@@ -94,4 +101,10 @@ variable "kvisor_values" {
   description = "List of YAML formatted string values for kvisor helm chart"
   type        = list(string)
   default     = []
+}
+
+variable "wait_for_cluster_ready" {
+  type        = bool
+  description = "Wait for cluster to be ready before finishing the module execution, this option requires `castai_api_token` to be set"
+  default     = false
 }
