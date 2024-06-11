3b65d6fdcda57ac17bdb494a81b5a5d9053e5ee5
Mike Norgate <mikenorgate@users.noreply.github.com>
2024-06-11 14:28:46 +0100

    feat: add autoscaler_policy_overrides support to module (#73)

--- variables.tf
+++ variables.tf
@@ -35,7 +35,13 @@ variable "gke_cluster_name" {
 
 variable "autoscaler_policies_json" {
   type        = string
-  description = "Optional json object to override CAST AI cluster autoscaler policies"
+  description = "Optional json object to override CAST AI cluster autoscaler policies. Deprecated, use `autoscaler_settings` instead."
+  default     = null
+}
+
+variable "autoscaler_settings" {
+  type        = any
+  description = "Optional Autoscaler policy definitions to override current autoscaler settings"
   default     = null
 }
 
